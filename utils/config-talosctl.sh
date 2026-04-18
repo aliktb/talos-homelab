b77cfd532749cc05f3587f80aeb19008c95e1a5a
Ali Khattab <aliktb@zohomail.eu>
2026-04-18 09:15:00 +0100

    Update script to simply talosctl config

--- utils/config-talosctl.sh
+++ utils/config-talosctl.sh
@@ -1,2 +1,9 @@
-talosctl config endpoint $CONTROL_PLANE_IP
-talosctl config node $CONTROL_PLANE_IP
+#!/usr/bin/env bash
+set -euo pipefail
+
+source config.env
+
+TARGET_NODE="${CONTROL_PLANE_IP:-$NODE}"
+
+talosctl config endpoint "$TARGET_NODE"
+talosctl config node "$TARGET_NODE"
